b4062eb5578cb5d5c9a098660bac4a8c696c334f
Олег Маслеников <Oleg.maslenikov.2014@mail.ru>
2023-02-18 00:50:25 +0300

    test2

--- two/factorial.py
+++ two/factorial.py
@@ -1,12 +1,15 @@
-print('Привет, Никита. Введи число для подсчета суммы факториалов')
-number = int(input())
+import math
 
-if number <= 4:
-    print('Попробуй ввести число больше')
+print('Привет, как тебя зовут?')
+first_name = str(input())
+print(f'{first_name.title()}, веди число больше 5 для подсчета суммы факториалов.')
+number = int(input())
+sum_fact = 0
+if number < 5:
+    print('Необходимо число больше 5')
 else:
-    fact = 1
-    score = 0
     for i in range(5, number+1):
-        fact = fact * i
-        score +=fact
-    print(f"Сумма факториалов равена - {score}")
+        fact = math.factorial(i)
+        sum_fact +=fact
+    print(f'Сумма факториалов {sum_fact}')
+
