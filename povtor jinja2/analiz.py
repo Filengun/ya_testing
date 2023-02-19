207919f6cec9ba806e745b83e91a000026df0053
Олег Маслеников <Oleg.maslenikov.2014@mail.ru>
2023-02-19 20:58:03 +0300

    peredelal zagrusky shablona

--- povtor jinja2/analiz.py
+++ povtor jinja2/analiz.py
@@ -5,15 +5,12 @@ import jinja2
 with open('data.json') as f:
     incoming_request = f.read() #передали json входящий
 
-with open('data2.json') as w:
-    template_base = w.read() #передали json шаблон
-
-
-template = jinja2.Template(template_base)
+template_base = jinja2.FileSystemLoader('')
+env = jinja2.Environment(loader=template_base)
+template = env.get_template('data2.json')
 incoming_request_1  = json.loads(incoming_request)
 print(incoming_request_1)
 variable = template.render(incoming_request_1=incoming_request_1)
-#print(incoming_request)
 print('-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_')
 print(variable)
 print('-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_')
